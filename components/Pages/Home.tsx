aa073b42acda1c65f22b01336087f7cea10a7ed0
Hridoy Ahmed <hridoyahmed150@gmail.com>
2024-06-10 12:34:30 +0600

    update

--- components/Pages/Home.tsx
+++ components/Pages/Home.tsx
@@ -54,7 +54,7 @@ export default function Home() {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span className="">khandakerhabibul@gmail.com</span>
+                  <span className="">hridoyahmed150@gmail.com</span>
                 </div>
                 <div className="flex gap-3 items-start text-sm">
                   <svg
@@ -71,7 +71,7 @@ export default function Home() {
                     <rect x={5} y={2} width={14} height={20} rx={2} ry={2} />
                     <line x1={12} y1={18} x2="12.01" y2={18} />
                   </svg>
-                  <span className="">+8801718509393</span>
+                  <span className="">+8801681101130</span>
                 </div>
                 <div className="flex gap-3 items-start text-sm">
                   <svg
@@ -115,11 +115,11 @@ export default function Home() {
                     <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
                   </svg>
                   <a
-                    href="https://www.linkedin.com/in/khandaker-habibul-amin/"
+                    href="https://www.linkedin.com/in/hridoyahmed150/"
                     target="_blank"
                     className=""
                   >
-                    linkedin.com/in/khandaker-habibul-amin
+                    linkedin.com/in/hridoyahmed150
                   </a>
                 </div>
                 <div className="flex gap-3 items-start text-sm">
@@ -135,11 +135,11 @@ export default function Home() {
                     <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
                   </svg>
                   <a
-                    href="https://github.com/khandakerhabibul"
+                    href="https://github.com/hridoyahmed150"
                     target="_blank"
                     className=""
                   >
-                    github.com/khandakerhabibul
+                    github.com/hridoyahmed150
                   </a>
                 </div>
               </div>
@@ -164,10 +164,16 @@ export default function Home() {
                 </div>
                 <div className="mt-6 flex gap-2 flex-wrap">
                   <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
+                    JavaScript
+                  </div>
+                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
                     TypeScript
                   </div>
                   <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    JavaScript
+                    Angular
+                  </div>
+                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
+                    Angular Material
                   </div>
                   <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
                     React.JS
@@ -182,29 +188,9 @@ export default function Home() {
                     React-Hook-Form
                   </div>
                   <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Storybook
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Shadcn
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
                     Tailwind-CSS
                   </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    MUI
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Redux
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Redux-Toolkit
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Zustand
-                  </div>
-                  <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
-                    Recharts
-                  </div>
+
                   <div className="text-white px-3 rounded text-sm py-1 font-medium bg-[#449399]">
                     HTML
                   </div>
@@ -338,9 +324,7 @@ export default function Home() {
             <div className="py-4 pb-14 pr-6">
               <div className="hidden md:block">
                 <div>
-                  <h1 className="mt-2 text-4xl font-semibold">
-                    Khandaker Habibul Amin
-                  </h1>
+                  <h1 className="mt-2 text-4xl font-semibold">Hridoy Ahmed</h1>
                   <h4 className="mt-2 text-primary-green font-semibold text-xl">
                     Software Engineer, Frontend
                   </h4>
